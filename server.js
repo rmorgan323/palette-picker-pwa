daf8c600281bb27ac79247a7c95c0fbcdd56ed74
Rob Morgan <rmorgan323@gmail.com>
2018-01-26 10:42:40 -0700

    add app.set in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
